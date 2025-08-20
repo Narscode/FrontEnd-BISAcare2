fe3b0ecb0dcea99711db652384b437a323e367cc
Nareswari Ayu Prabowo <144470962+Narscode@users.noreply.github.com>
2025-08-20 17:54:35 +0700

    Add or update metro.config.js file

---
+++ metro.config.js
@@ -0,0 +1,16 @@
+/**
+ * Metro configuration for React Native
+ * https://facebook.github.io/metro/
+ *
+ * @format
+ */
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
